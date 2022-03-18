dbedbfecb906ae739544cdb76f12036c5fe39b98
Hermione Allognon <allognonhermione@gmail.com>
2022-03-18 16:56:28 +0100

    fix alt to image

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,11 @@
 import style from './Weather.module.css';
 
 const Weather = (props) => {
-    const {weather,isDayTime,city}=props
+    const {weather,city}=props
     return (
         <div className={style.content}>
             <div className={style.city}>{city}</div>
-            {weather && weather.weather && weather.weather[0] && weather.weather[0].icon && <img className={style.img} src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`} />}
+            {weather && weather.weather && weather.weather[0] && weather.weather[0].description && weather.weather[0].icon && <img className={style.img} src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`} alt={weather.weather[0].description} />}
             {weather && weather.main && weather.main.temp && <div className={style.size}>{weather.main.temp}°C</div>}
             {weather && weather.weather && weather.weather[0] && weather.weather[0].description && <div className={style.description}>{weather.weather[0].description}</div>}
             
